691f224cab863d7b2d34992068e9295a3441ff97
Lucas Oliveira <49119918+lucaslourenco189@users.noreply.github.com>
2022-03-10 16:44:07 -0300

    Update index.js
    Adicionado os campos de usuário e senha.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,20 +4,29 @@ import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-nativ
 import * as Animatable from 'react-native-animatable';
 
 export default function SignIn() {
- return (
-   <View style={styles.container}>
-     <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
-       <Text style={styles.message}>Bem-vindo(a)</Text>
-     </Animatable.View>
-     <Animatable.View animation="fadeInUp" style={styles.containerForm}>
-
-     </Animatable.View>
-   </View>
+  return (
+    <View style={styles.container}>
+      <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+        <Text style={styles.message}>Bem-vindo(a)</Text>
+      </Animatable.View>
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+        <Text style={styles.title}>Email</Text>
+        <TextInput
+          placeholder="Digite seu email..."
+          style={styles.input}
+        />
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+          placeholder="Digite sua senha"
+          style={styles.input}
+        />
+      </Animatable.View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
 
   }
 })
