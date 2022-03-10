32bda2567b3d3ef141e0f2a8cfcbe14d819477d9
Lucas Oliveira <49119918+lucaslourenco189@users.noreply.github.com>
2022-03-10 17:14:34 -0300

    Update index.js

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -7,12 +7,12 @@ export default function SignIn() {
   return (
     <View style={styles.container}>
       <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
-        <Text style={styles.message}>Bem-vindo(a)</Text>
+        <Text style={styles.message}>Bem-vindo(a) ao Auto Gestão do Aleator Software</Text>
       </Animatable.View>
       <Animatable.View animation="fadeInUp" style={styles.containerForm}>
         <Text style={styles.title}>Email</Text>
         <TextInput
-          placeholder="Digite seu email..."
+          placeholder="Digite seu email"
           style={styles.input}
         />
         <Text style={styles.title}>Senha</Text>
@@ -20,6 +20,12 @@ export default function SignIn() {
           placeholder="Digite sua senha"
           style={styles.input}
         />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText}>Esqueceu a senha? Clique aqui!</Text>
+        </TouchableOpacity>
       </Animatable.View>
     </View>
   );
@@ -27,6 +33,56 @@ export default function SignIn() {
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    backgroundColor: '#194C8B',
+  },
+  containerHeader:{
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%'
+  },
+  message:{
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  containerForm:{
+    backgroundColor: '#FFF',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+  title:{
+    fontSize: 20,
+    marginTop: 28
+  },
+  input:{
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16
+  },
+  button:{
+    backgroundColor: '#194C8B',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  buttonRegister:{
+    marginTop: 14,
+    alignSelf: 'center',
+  },
+  registerText:{
+    color: '#A1A1A1'
   }
 })
